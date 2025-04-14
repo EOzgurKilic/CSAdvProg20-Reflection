012c82ea28ced62c90d29d8bc33b8e5c4d892498
orkhaner <orkhanrrahimli@gmail.com>
2025-04-14 09:49:48 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CSAdvProg20_Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Reflection
+        //A feature that allows us to access any concepts' (class, struct, delegate, method, etc.) info and manipulate them in runtime.
+        
+        //Reflection has 3 main usage purposes;
+        //-Type Info Check: We can review any concepts' name, properties, methods, fields, etc. info.
+        //Thus, we can dynamically take actions based on this info by accessing them in runtime.
+        //-New Type Creation or Load One: Reflection makes it possible to create new types or load the ones from .dll files.
+        //-Running Members Dynamically: We can call or run members such as methods and properties via reflection
+        
+        
+    }
+}
